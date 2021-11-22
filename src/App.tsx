8e7b35e5328f9473f004f40c0feeddc512756f02
lucas vinicius <lucasviniciusaalves@gmail.com>
2021-11-22 19:26:49 -0300

    add: Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import { GlobalStyle } from './styles/global';
+
 function App() {
   return (
-    <h1>My App</h1>
+    <>
+      <GlobalStyle />
+      <h1>My App</h1>
+    </>
   );
 }
 
